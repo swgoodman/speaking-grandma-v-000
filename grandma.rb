3908c96a5008f8b21aaa20bc9e48e22eaa037e9a
Spencer Goodman <spencerwgoodman@icloud.com>
2018-04-24 01:15:11 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -5,9 +5,9 @@ def speak_to_grandma(phrase)
   if phrase = "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
   elsif phrase === phrase.capitalize
-    puts "HUH?! SPEAK UP, SONNY!"
-  else
     puts "NO, NOT SINCE 1938!"
+  else phrase = phrase
+    puts "HUH?! SPEAK UP, SONNY!"
   end
 end
 
