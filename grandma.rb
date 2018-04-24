f7ccc1e87cf9198708c4e9826baeefcf47d3831a
Spencer Goodman <spencerwgoodman@icloud.com>
2018-04-24 01:17:44 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -4,7 +4,7 @@
 def speak_to_grandma(phrase)
   if phrase = "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
-  elsif 
+  elsif
     puts "NO, NOT SINCE 1938!"
   else 
     puts "HUH?! SPEAK UP, SONNY!"
