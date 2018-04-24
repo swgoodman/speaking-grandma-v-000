7fc22539dc4f28ea2f02a854e6bc74754e405389
Spencer Goodman <spencerwgoodman@icloud.com>
2018-04-24 01:23:54 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,10 +1,10 @@
 # Write a speak_to_grandma method.
 require 'pry'
 
-binding.pry
+
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
-    puts "I LOVE YOU TOO PUMPKIN!"
+    return "I LOVE YOU TOO PUMPKIN!"
   elsif phrase == phrase.upcase
     puts "NO, NOT SINCE 1938!"
   else
